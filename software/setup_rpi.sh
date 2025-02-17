d37792c29150d5fa5ac1068e53ac55102aed9a06
Tyler Bowers <tylerebowers@gmail.com>
2025-02-17 13:19:40 -0500

    Update setup_rpi.sh

--- software/setup_rpi.sh
+++ software/setup_rpi.sh
@@ -9,4 +9,4 @@ sudo systemctl enable NetworkManager.service
 wget https://github.com/tylerebowers/Flipdot-Array/archive/refs/heads/main.zip
 unzip main.zip
 cd Flipdot-Array-main/software/
-sudo apt install python3-numpy
+sudo apt install python3-numpy -y
