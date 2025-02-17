95fc36b272b537a3dbb88c9ca844e08c01f010f7
Tyler Bowers <tylerebowers@gmail.com>
2025-02-17 13:13:01 -0500

    Update setup_rpi.sh

--- software/setup_rpi.sh
+++ software/setup_rpi.sh
@@ -1,10 +1,10 @@
-sudo apt-get update
-sudo apt upgrade
+sudo apt update
+sudo apt upgrade -y
 wget https://davesteele.github.io/comitup/deb/davesteele-comitup-apt-source_1.2_all.deb
 sudo dpkg -i davesteele-comitup-apt-source*.deb
-sudo apt-get update
-sudo apt upgrade
-sudo apt install comitup
+sudo apt update
+sudo apt upgrade -y
+sudo apt install comitup -y
 sudo systemctl enable NetworkManager.service
 wget https://github.com/tylerebowers/Flipdot-Array/archive/refs/heads/main.zip
 unzip main.zip
