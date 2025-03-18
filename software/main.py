9c530f5d781e17a8636cd6ce6eb9d7d6a623d769
Tyler Bowers <tylerebowers@gmail.com>
2025-03-17 23:03:42 -0700

    work on software

--- software/main.py
+++ software/main.py
@@ -1,70 +1,24 @@
-#from display import Display
-from display_simulator import Display
-from libraries import *
-import numpy as np
-import datetime
-import time
+from interface.display_simulator import Display
+#from interface.display import Display
+import runners
 
-
-def clock(d, hours_24=False):
-    shown_time = datetime.datetime.now() - datetime.timedelta(minutes=1)
-    while True:
-            now = datetime.datetime.now()
-            if now.minute != shown_time.minute:
-                shown_time = now
-                d.write_display(numbers_4x7[int(now.hour if hours_24 else int(now.strftime("%I")) / 10)], bitwize=True)
-                d.write_display(numbers_4x7[int(now.hour if hours_24 else int(now.strftime("%I")) % 10)], start_x=5, bitwize=True)
-                d.write_display(special_8[0], start_x=10, bitwize=True)
-                d.write_display(numbers_4x7[int(now.minute / 10)], start_x=12, bitwize=True)
-                d.write_display(numbers_4x7[now.minute % 10], start_x=17, bitwize=True)
-            time.sleep(1)
-
-def game_of_life(d):
-    #d.write_display(np.random.randint(2, size=(21,7)))
-
-    def count_neighbors(x, y):
-        total = 0
-        for i in range(x - 1, x + 2):
-            for j in range(y - 1, y + 2):
-                if (i == x and j == y) or i < 0 or j < 0 or i >= d.shown.shape[0] or j >= d.shown.shape[1]:
-                    continue
-                total += d.shown[i][j]
-        return total
-
-    def update_grid():
-        for x in range(d.shown.shape[0]):
-            for y in range(d.shown.shape[1]):
-                neighbors = count_neighbors(x, y)
-                if d.shown[x][y] == 1:
-                    if neighbors < 2 or neighbors > 3:
-                        d.write_dot(x, y, False)
-                else:
-                    if neighbors == 3:
-                        d.write_dot(x, y, True)
-
-    while True:
-        d.write_display(np.random.randint(2,size=(21,7)))
-        for i in range(60):
-            update_grid()
-            time.sleep(0.5)
-
-def pong(d):
-    pass
-
-def temperature(d):
-    pass
-
-def date(d):
-    pass
 
 d = Display()
-time.sleep(1)
-d.all_on()
-time.sleep(2)
-d.all_off()
-time.sleep(1)
-#clock(d)
-game_of_life(d)
-#time.sleep(5)
-#d.all_on()
+#runners.self_test(d)
 
+"""
+modes:
+"clock": (runners.Clock, (False,))
+"game_of_life": (runners.GameOfLife, (2,))
+"""
+
+runner = None
+new_mode = (runners.GameOfLife, (2,))#(runners.Clock, (False,))
+
+while True:
+    if new_mode is not None:
+        runner = new_mode[0](d, *new_mode[1])
+        new_mode = None
+    else:
+        runner.update()
+    
