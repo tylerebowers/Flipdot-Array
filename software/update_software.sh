6e657d8df15396e45846b5a939f1f1d961ee09ba
Tyler Bowers <tylerebowers@gmail.com>
2025-03-29 18:25:55 -0400

    Update update_software.sh

--- software/update_software.sh
+++ software/update_software.sh
@@ -5,5 +5,14 @@ sudo systemctl stop flipdots.service
 echo "Updating software"
 git fetch --all
 git reset --hard origin/main
+
+read -t 10 -r -p "Recompile interface? [y/N] (continuing in 10s) " response 
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    cd interface
+    c++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) display.cpp -o display$(python3-config --extension-suffix) -lwiringPi
+    cd ..
+fi
+
 echo "Starting service"
 sudo systemctl start flipdots.service
