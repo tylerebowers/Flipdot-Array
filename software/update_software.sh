90449b28b3765d3ab972f44659e50fda6c2c0211
Tyler Bowers <tylerebowers@gmail.com>
2025-03-31 11:24:42 -0400

    Update update_software.sh

--- software/update_software.sh
+++ software/update_software.sh
@@ -1,10 +1,13 @@
 # This script updates to the latest flipdot software.
 
 echo "Updating software"
+
 INTERFACE="interface/display.cpp"
 before=$(stat -c %Y "$INTERFACE")
+
 git fetch --all
 git reset --hard origin/main
+
 after=$(stat -c %Y "$INTERFACE")
 
 if [[ "$before" -ne "$after" ]]; then
@@ -13,8 +16,8 @@ if [[ "$before" -ne "$after" ]]; then
 else
     read -t 5 -r -p "Recompile interface? [y/N] (continuing in 5s) " response 
 fi
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-then
+
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
     echo "Compiling interface"
     cd interface
     c++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) display.cpp -o display$(python3-config --extension-suffix) -lwiringPi
