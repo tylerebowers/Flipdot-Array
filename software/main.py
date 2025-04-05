48f4f5da96cc222dc6ab30668a52147e4075e123
Tyler Bowers <tylerebowers@gmail.com>
2025-04-05 14:10:05 -0400

    Update main.py

--- software/main.py
+++ software/main.py
@@ -58,7 +58,7 @@ class WebServer:
 
         @self.app.post("/mode")
         async def set_mode(request: Request):
-            if self.websocket_client is not None:
+            if self.websocket_client is None:
                 global new_mode
                 new_mode = await request.json()
             print("Received: ", new_mode)
